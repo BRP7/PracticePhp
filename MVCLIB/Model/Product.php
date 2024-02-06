74aebcc81bb6c92ebca5227102a88f3cc2d97aa1
BRP7 <119129720+BRP7@users.noreply.github.com>
2024-02-06 23:24:35 +0530

    The First MVC and Dynamic Class

---
+++ MVCLIB/Model/Product.php
@@ -0,0 +1,68 @@
+<?php
+class Model_Product extends Model_Abstract
+{
+    public $tableName = "ccc_product";
+
+    public function __construct()
+    {
+        // echo "I'm here";
+        // echo get_class($this);
+    }
+
+    public function save($data)
+    {
+        echo "<pre>";
+        $sql = $this->getQueryBuilder()->insert(
+                $this->tableName,
+                $data
+            );
+         $this->getQueryBuilder()->exec($sql);
+        // echo $sql;
+        // print_r($data);
+    }
+
+        public function update($data,$condition)
+    {
+        echo "<pre>";
+        $sql = $this->getQueryBuilder()->update(
+                $this->tableName,
+                $data,
+                $condition
+            );
+         $this->getQueryBuilder()->exec($sql);
+        // echo $sql;
+        // print_r($data);
+    }
+        public function del($condition)
+    {
+        echo "<pre>";
+        $sql = $this->getQueryBuilder()->delete(
+                $this->tableName,
+                $condition
+            );
+         $this->getQueryBuilder()->exec($sql);
+        // echo $sql;
+        // print_r($data);
+    }
+    public function show($columns = "*", $condition = [])
+{
+    // Ensure $columns is a string
+    if (!is_string($columns)) {
+        $columns = implode(', ', $columns);
+    }
+
+    if (is_array($condition) && !empty($condition)) {
+        $conditions = [];
+        foreach ($condition as $key => $value) {
+            $conditions[] = "$key = '$value'";
+        }
+        $condition = implode(' AND ', $conditions);
+    }
+
+    $sql = $this->getQueryBuilder()->selectQuery($this->tableName, $columns, $condition);
+    $result = $this->getQueryBuilder()->exec($sql);
+
+    return $this->getQueryBuilder()->fetch_asso($result);
+}
+    
+}
---
+++ MVCLIB/index.php
@@ -0,0 +1,228 @@
+<?php
+
+// include "Lib/autoload.php";
+
+// $request = new Model_Request();
+// $productModel = new Model_Product();
+
+// if (!$request->isPost()) {
+//     // Display the list of products
+//     $products = $productModel->show();
+//     $productView = new View_Product();
+//     echo $productView->toHtml($products);
+// } else {
+//     // Handle form submission
+//     $postData = $request->getPostData('pdata');
+    
+//     if (isset($postData['submit'])) {
+//         // Insert or update based on the presence of 'product_id'
+//         if (isset($postData['product_id']) && !empty($postData['product_id'])) {
+//             $productId = $postData['product_id'];
+//             unset($postData['product_id']);
+//             $productModel->update($postData, ['product_id' => $productId]);
+//         } else {
+//             $productModel->save($postData);
+//         }
+
+//         // Redirect to index.php after successful submission
+//         header("Location: index.php");
+//         exit();
+//     }
+// }
+
+// // Handle edit and delete actions
+// if (isset($_GET['action']) && !empty($_GET['action'])) {
+//     $action = $_GET['action'];
+//     $productId = $_GET['id'];
+
+//     if ($action === 'edit') {
+//         // Fetch the product details and display the form for editing
+//         $productDetails = $productModel->show('*', ['product_id' => $productId]);
+//         $productView = new View_Product();
+//         echo $productView->createForm($productDetails[0]);
+//         exit();
+//     } elseif ($action === 'delete') {
+//         // ... existing code
+//     }
+// }
+
+
+// <?php
+
+// include "Lib/autoload.php";
+
+// $request = new Model_Request();
+// $productModel = new Model_Product();
+
+// if (!$request->isPost()) {
+//     // Display the list of products
+//     $products = $productModel->show();
+//     $productView = new View_Product();
+//     echo $productView->toHtml($products);
+// } else {
+//     // Handle form submission
+//     $postData = $request->getPostData('pdata');
+    
+//     if (isset($postData['submit'])) {
+//         // Insert or update based on the presence of 'product_id'
+//         if (isset($postData['product_id']) && !empty($postData['product_id'])) {
+//             $productId = $postData['product_id'];
+//             unset($postData['product_id']);
+//             $productModel->update($postData, ['product_id' => $productId]);
+//         } else {
+//             $productModel->save($postData);
+//         }
+
+//         // Redirect to index.php after successful submission
+//         header("Location: index.php");
+//         exit();
+//     }
+// }
+
+// // Handle edit and delete actions
+
+//     if (isset($_GET['action']) && !empty($_GET['action'])) {
+//         $action = $_GET['action'];
+//         $productId = $_GET['id'];
+    
+//         if ($action === 'edit') {
+//             // Fetch the product details and display the form for editing
+//             $productDetails = $productModel->show('*', ['product_id' => $productId]);
+//             echo $productView->createForm($productDetails[0]);
+//             exit();
+//         } elseif ($action === 'delete') {
+//             // Delete the product and redirect to index.php
+//             $productModel->del(['product_id' => $productId]);
+//             header("Location: index.php");
+//             exit();
+//         }
+//     }
+
+// <?php
+
+// include "Lib/autoload.php";
+
+// $request = new Model_Request();
+// $productModel = new Model_Product();
+
+// // Handle form submission
+// if ($request->isPost()) {
+//     $postData = $request->getPostData('pdata');
+
+//     if (isset($postData['submit'])) {
+//         // Insert or update based on the presence of 'product_id'
+//         if (isset($postData['product_id']) && !empty($postData['product_id'])) {
+//             $productId = $postData['product_id'];
+//             unset($postData['product_id']);
+//             $productModel->update($postData, ['product_id' => $productId]);
+//         } else {
+//             $productModel->save($postData);
+//         }
+
+//         // Redirect to index.php after successful submission
+//         header("Location: index.php");
+//         exit();
+//     }
+// }
+
+// // Handle edit and delete actions
+// if (isset($_GET['action']) && !empty($_GET['action'])) {
+//     $action = $_GET['action'];
+//     $productId = $_GET['id'];
+
+//     if ($action === 'edit') {
+//         // Fetch the product details and display the form for editing
+//         $productDetails = $productModel->show('*', ['product_id' => $productId]);
+//         $productView = new View_Product();
+//         echo $productView->createForm($productDetails[0]);
+//         exit();
+//     } elseif ($action === 'delete') {
+//         // Delete the product and redirect to index.php
+//         $productModel->del(['product_id' => $productId]);
+//         header("Location: index.php");
+//         exit();
+//     }
+// }
+
+// // Display the list of products
+// $products = $productModel->show();
+// $productView = new View_Product();
+// echo $productView->toHtml($products);
+
+
+
+
+
+// <?php
+
+// <?php
+
+include "Lib/autoload.php";
+
+$request = new Model_Request();
+$productModel = new Model_Product();
+$productView = new View_Product();
+
+// Handle form submission
+echo "Hello";
+// print_r($request->getPostData());
+if ($request->isPost()) {
+    $postData = $request->getPostData('pdata');
+    // print_r($postData);
+    // var_dump(isset($postData['submit']));
+    
+    if (($request->getPostData('submit'))) {
+        // Insert or update based on the presence of 'product_id'
+        if (!empty($postData['product_id'])) {
+            $productId = $postData['product_id'];
+            // unset($postData['product_id']); 
+            $productModel->update($postData, ['product_id' => $productId]);
+            header("Location: index.php");   
+            // Redirect to index.php after successful submission
+            // header("Location: index.php");
+            // exit();
+        }
+        else{
+            $productModel->save($postData);
+        }
+    }
+}
+
+// Handle edit and delete actions
+if (isset($_GET['action']) && !empty($_GET['action'])) {
+    $action = $_GET['action'];
+    $productId = $_GET['id'];
+    $postData = $request->getPostData('pdata');
+    if ($action === 'edit') {
+        // Fetch the product details and display the form for editing
+        $productDetails = $productModel->show('*', ['product_id' => $productId]);
+        echo $productView->createForm($productDetails[0]); 
+        // // if($action === 'edit') {
+            // //     print_r($productId);
+            // //     die();
+            // unset($postData['product_id']);
+            // $productModel->update($postData, ['product_id' => $productId]);
+            // header("Location: index.php");
+            // }
+            
+            exit();
+        }
+        elseif ($action === 'delete') {
+            // Delete the product and redirect to index.php
+        $productModel->del(['product_id' => $productId]);
+        header("Location: index.php");
+        exit();
+    }elseif ($action === 'add'){
+        
+        echo $productView->createForm(); 
+        // if(isset($postData['submit'])){
+            // $productModel->save($postData);
+        // }
+    }
+}
+
+// Display the list of products
+$products = $productModel->show();
+echo $productView->toHtml($products);
+
+?>
